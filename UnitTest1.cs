e925290471124af0317608a3498fbeaf28a39cd2
turgor <maxac153@gmail.com>
2023-09-27 23:42:56 +0500

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumUsersSharp;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://users.bugred.ru/");
+
+            driver.FindElement(By.XPath("//span[text()='Войти']")).Click();
+
+            driver.FindElement(By.XPath("//input[@required=''][@name='name']")).SendKeys("Tur123");
+            driver.FindElement(By.XPath("//input[@required=''][@name='email']")).SendKeys("verygood@gmail.com");
+            driver.FindElement(By.XPath("//input[@name='password'][@required='']")).SendKeys("123");
+
+            driver.FindElement(By.XPath("//input[@value='Зарегистрироваться']")).Click();
+
+            driver.Close();
+            driver.Quit();
+        Assert.Pass();
+    }
+}
